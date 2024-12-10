625917ee31d957950bbda989a6bb13d1bcbfd357
minhnhat1610 <161536740+minhnhat1610@users.noreply.github.com>
2024-12-10 18:56:11 +0700

    Add files via upload

---
+++ ss2-2c.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+ 
+    int num1 = 10; 
+    int num2 = 20; 
+
+   
+    int sum = num1 + num2;
+
+    printf("Tổng của %d và %d là: %d\n", num1, num2, sum);
+
+    return 0;
+}
